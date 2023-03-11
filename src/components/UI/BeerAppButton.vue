933823499b92e3d9afa8a7e2a2fc0d2e13460a7f
Piotr Rajtar <piotrek.rajtar@gmail.com>
2023-03-11 21:24:03 +0100

    add beer button

---
+++ src/components/UI/BeerAppButton.vue
@@ -0,0 +1,33 @@
+<template>
+  <button :class="style.button">
+    <slot />
+  </button>
+</template>
+
+<style lang="scss" module="style">
+@use '@/styles/colors.scss';
+@use '@/styles/fonts.scss';
+@use '@/styles/spacings.scss';
+
+.button {
+  background: radial-gradient(colors.$yellow-light, colors.$yellow-dark);
+  width: 200px;
+  height: 60px;
+  border-radius: spacings.$spacing-unit;
+  border: 0;
+  outline: none;
+  box-shadow: 7px 6px 28px 1px colors.$button-shadow;
+  font-weight: fonts.$font-bold;
+  cursor: pointer;
+  transition: 0.2s all;
+            
+  &:hover {
+    border: 1px solid colors.$black;
+  }
+
+  &:active {
+    transform: scale(0.98);
+    box-shadow: 3px 2px 22px 1px colors.$button-shadow;
+  }
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,24 @@
 <script lang="ts" setup>
+import BeerAppButton from '../components/UI/BeerAppButton.vue';
 </script>
 
 <template>
-  HOME VIEW
+  <h2 :class="style.header">Beer catalogue browser</h2>
+  <section :class="style.sectionContainer">
+    <BeerAppButton>Start browsing</BeerAppButton>
+  </section>
 </template>
 
 <style lang="scss" module="style">
+
+.header {
+  text-align: center;
+  text-decoration: underline;
+  margin: 50px 0;
+}
+
+.sectionContainer {
+  display: flex;
+  justify-content: center;
+}
 </style>
