6f6c459dd4fb624579fdd0eafd8893ff7dc80b24
Piotr Rajtar <piotrek.rajtar@gmail.com>
2023-03-12 17:24:02 +0100

    add beer button functionality

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
+<template>
+  <BeerAppHeader />
+  <RouterView />
+</template>
+
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
 
 import BeerAppHeader from './components/layout/BeerAppHeader.vue';
 </script>
-
-<template>
-  <BeerAppHeader />
-  <RouterView />
-</template>
 
 <style lang="scss" module="style">
 @use '@/styles/fonts.scss';
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,45 @@
-<script lang="ts" setup>
-import BeerAppButton from '../components/UI/BeerAppButton.vue';
-</script>
-
 <template>
   <h2 :class="style.header">Beer catalogue browser</h2>
   <section :class="style.sectionContainer">
-    <BeerAppButton>Start browsing</BeerAppButton>
+    <BeerAppButton @click="debouncedOnLoadInitialData">Start browsing</BeerAppButton>
   </section>
 </template>
 
-<style lang="scss" module="style">
+<script lang="ts" setup>
+import { computed, ref } from 'vue'
+import type { ComputedRef, Ref } from 'vue'
+import { debounce } from 'lodash'
 
+import BeerAppButton from '../components/UI/BeerAppButton.vue'
+import { useBeerStore } from '../stores/beer'
+import { SortDirection } from '../typings/typings'
+import type { SortBy, QueryParams } from '../typings/typings'
+
+const store = useBeerStore()
+
+const pageNumber = ref(1)
+
+const queryParams: ComputedRef<QueryParams> = computed(() => ({
+  page: pageNumber
+}));
+
+const sortBy: Ref<SortBy | null> = ref(null)
+const sortDirection: Ref<SortDirection> = ref(SortDirection.NONE)
+
+const setTableInitialState = (): void => {
+  sortBy.value = null
+  sortDirection.value = SortDirection.NONE
+  pageNumber.value = 1
+}
+
+const onLoadInitialData = async (): Promise<void> => {
+  await store.loadInitialBeersData(queryParams)
+}
+
+const debouncedOnLoadInitialData = debounce(onLoadInitialData, 300)
+</script>
+
+<style lang="scss" module="style">
 .header {
   text-align: center;
   text-decoration: underline;
