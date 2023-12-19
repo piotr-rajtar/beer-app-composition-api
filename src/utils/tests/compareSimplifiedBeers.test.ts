919723a3faf4b9849cebd8f732f697af1a0d5428
Piotr Rajtar <piotrek.rajtar@gmail.com>
2023-12-19 19:16:36 +0100

    add tests for compareSimplifiedBeers function

---
+++ src/utils/tests/compareSimplifiedBeers.test.ts
@@ -0,0 +1,76 @@
+import { beforeEach, describe, expect, it } from 'vitest';
+
+import type { SimplifiedBeer } from '../../typings';
+import { SortDirection, TableHeaderKey } from '../../typings';
+
+import { compareSimplifiedBeers } from '../';
+
+import { mockedBeersData } from './mockedBeersData';
+
+describe('compareSimplifiedBeer function', () => {
+  let testData: SimplifiedBeer[];
+
+  beforeEach(() => {
+    testData = [...mockedBeersData];
+  });
+
+  it('sorts beer data correctly with asc order by beer name', () => {
+    testData.sort(
+      compareSimplifiedBeers({
+        sortBy: TableHeaderKey.NAME,
+        sortDirection: SortDirection.ASC,
+      })
+    );
+    expect(testData[0].name).toBe('Buzz');
+  });
+
+  it('sorts beer data correctly with desc order by beer name', () => {
+    testData.sort(
+      compareSimplifiedBeers({
+        sortBy: TableHeaderKey.NAME,
+        sortDirection: SortDirection.DESC,
+      })
+    );
+    expect(testData[0].name).toBe('Trashy Blonde');
+  });
+
+  it('sorts beer data correctly with asc order by first brewed date', () => {
+    testData.sort(
+      compareSimplifiedBeers({
+        sortBy: TableHeaderKey.FIRST_BREWED,
+        sortDirection: SortDirection.ASC,
+      })
+    );
+    expect(testData[0].first_brewed).toBe('09/2007');
+  });
+
+  it('sorts beer data correctly with desc order by first brewed date', () => {
+    testData.sort(
+      compareSimplifiedBeers({
+        sortBy: TableHeaderKey.FIRST_BREWED,
+        sortDirection: SortDirection.DESC,
+      })
+    );
+    expect(testData[0].first_brewed).toBe('04/2008');
+  });
+
+  it('sorts beer data correctly with asc order by other category than name and first brewed date', () => {
+    testData.sort(
+      compareSimplifiedBeers({
+        sortBy: TableHeaderKey.EBC,
+        sortDirection: SortDirection.ASC,
+      })
+    );
+    expect(testData[0].ebc).toBe(15);
+  });
+
+  it('sorts beer data correctly with desc order by other category than name and first brewed date', () => {
+    testData.sort(
+      compareSimplifiedBeers({
+        sortBy: TableHeaderKey.EBC,
+        sortDirection: SortDirection.DESC,
+      })
+    );
+    expect(testData[0].ebc).toBe(20);
+  });
+});
---
+++ src/utils/tests/mockedBeersData.ts
@@ -0,0 +1,22 @@
+import type { SimplifiedBeer } from '../../typings';
+
+export const mockedBeersData: SimplifiedBeer[] = [
+  {
+    id: 1,
+    name: 'Buzz',
+    first_brewed: '09/2007',
+    abv: 4.5,
+    ibu: 60,
+    ebc: 20,
+    ph: 4.4,
+  },
+  {
+    id: 2,
+    name: 'Trashy Blonde',
+    first_brewed: '04/2008',
+    abv: 4.1,
+    ibu: 41.5,
+    ebc: 15,
+    ph: 4.4,
+  },
+];
