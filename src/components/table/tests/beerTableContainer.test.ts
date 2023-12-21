64230071123f9c8a27c5741c67781eea89143473
Piotr Rajtar <piotrek.rajtar@gmail.com>
2023-12-21 19:49:18 +0100

    add test to beer table container

---
+++ src/components/table/tests/beerTableContainer.test.ts
@@ -0,0 +1,29 @@
+import { describe, expect, it, vi } from 'vitest';
+import { shallowMount } from '@vue/test-utils';
+
+import BeerTableContainer from '../BeerTableContainer.vue';
+
+vi.mock('vue-i18n', () => ({
+  useI18n: () => ({ t: (key: string) => key }),
+}));
+
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: vi.fn().mockImplementation((query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: vi.fn(),
+    removeListener: vi.fn(),
+    addEventListener: vi.fn(),
+    removeEventListener: vi.fn(),
+    dispatchEvent: vi.fn(),
+  })),
+});
+
+describe('BeerTableContainer.vue', () => {
+  it('renders correctly', () => {
+    const wrapper = shallowMount(BeerTableContainer);
+    expect(wrapper).toBeTruthy();
+  });
+});
