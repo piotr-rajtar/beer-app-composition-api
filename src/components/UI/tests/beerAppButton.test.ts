2bf7b17d1ee838d1c3797aa5e4b94853a914742f
Piotr Rajtar <piotrek.rajtar@gmail.com>
2024-01-19 12:42:06 +0100

    add test to beerAppButton

--- src/components/UI/tests/beerAppButton.test.ts
+++ src/components/UI/tests/beerAppButton.test.ts
@@ -1,11 +1,22 @@
-import { describe, expect, it } from 'vitest';
-import { shallowMount } from '@vue/test-utils';
+import { beforeEach, describe, expect, it } from 'vitest';
+import { VueWrapper, shallowMount } from '@vue/test-utils';
 
 import BeerAppButton from '../BeerAppButton.vue';
 
 describe('BeerAppButton.vue', () => {
+  let wrapper: VueWrapper;
+
+  beforeEach(() => {
+    wrapper = shallowMount(BeerAppButton, {
+      slots: { default: 'Content' },
+    });
+  });
+
   it('renders correctly', () => {
-    const wrapper = shallowMount(BeerAppButton);
     expect(wrapper).toBeTruthy();
+  });
+
+  it('displays slot content', () => {
+    expect(wrapper.text()).toBe('Content');
   });
 });
