1b13b3f3f09ee75e8e3d6467f199bcc046f62956
Piotr Rajtar <piotrek.rajtar@gmail.com>
2024-02-12 14:51:30 +0100

    amend filter interface

--- src/typings/global.types.ts
+++ src/typings/global.types.ts
@@ -28,10 +28,9 @@ export interface Filters {
   abv_gt?: number;
   abv_lt?: number;
   beer_name?: string;
-  brewed_after?: string;
-  brewed_before?: string;
   ebc_gt?: number;
   ebc_lt?: number;
+  food?: string;
   ibu_gt?: number;
   ibu_lt?: number;
 }
